4f819485e875735290602d95b0ae2ec99e062ff6
henriquesenaribeiro <henriqueribeiro@usf.edu>
2025-02-02 22:06:30 -0500

    Task 2 - Modifying Customer Orders.

--- cc_3.js
+++ cc_3.js
@@ -5,3 +5,10 @@ prices.push(100); // Add a new price
 prices.shift(); // Remove the first price
 console.log("Updated prices:", prices); // Log updated prices
 
+// Task 2: Modifying Customer Orders
+
+let orders = [20, 35, 50, 65, 80];
+orders[2] += 7; // Increased the third order by 7, so from 35 to 42
+let totalOrders = orders.reduce((total, num) => total + num, 0); // Calculated total # of orders
+console.log("Updated Orders:", orders);
+console.log("Total Order Count:", totalOrders);
