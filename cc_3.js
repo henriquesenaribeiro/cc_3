22e729ddb6f7225163df2a7ddd06aba3069f8315
henriquesenaribeiro <henriqueribeiro@usf.edu>
2025-02-02 22:09:55 -0500

    Task 3 - Employee Performance Tracking.

--- cc_3.js
+++ cc_3.js
@@ -12,3 +12,14 @@ orders[2] += 7; // Increased the third order by 7, so from 35 to 42
 let totalOrders = orders.reduce((total, num) => total + num, 0); // Calculated total # of orders
 console.log("Updated Orders:", orders);
 console.log("Total Order Count:", totalOrders);
+
+// Task 3: Employee Performance Tracking
+let employee = {
+    name: "Eduardo Pio Ribery Junior",
+    role: "Banker",
+    performanceScore: 90,
+    isActive: true
+    };
+    employee.performanceScore = 90; // Updated performance score from 90 to 99
+    employee.promotionEligible = true; // Included a new boolean property to indicate promotion eligibility and set it to true.
+    console.log("Updated Employee Performance Tracking:", employee);
